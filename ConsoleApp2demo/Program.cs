2ebc2dcbd8a4037d1798d777ef17bb4e96e91ebe
Manikanta-GH <92135075+Manikanta-GH@users.noreply.github.com>
2021-10-16 12:09:31 +0530

    Update Program.cs
    added line for vs

--- ConsoleApp2demo/Program.cs
+++ ConsoleApp2demo/Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleApp2demo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World for github");
+            console.writeline("hello world for vs");
             Console.ReadLine();
         }
     }
