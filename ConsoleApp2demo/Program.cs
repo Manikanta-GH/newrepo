81025a7261823c9f09ef2a2cf1db4273687e143e
Pilli <manikanta-reddy.pilli@capgemini.com>
2021-10-16 12:02:31 +0530

    add consoleapp2 to github

---
+++ ConsoleApp2demo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp2demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World for github");
+            Console.ReadLine();
+        }
+    }
+}
