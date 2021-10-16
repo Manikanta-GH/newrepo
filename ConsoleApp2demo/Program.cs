8fb9cb98ebb08c40263d2a673d96befa6734520f
Manikanta-GH <92135075+Manikanta-GH@users.noreply.github.com>
2021-10-16 12:18:23 +0530

    Update Program.cs
    added j var

--- ConsoleApp2demo/Program.cs
+++ ConsoleApp2demo/Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleApp2demo
         static void Main(string[] args)
         {
             int i;
+            int j;
             Console.WriteLine("Hello World for github");
             Console.WriteLine("hello world for vs");
             Console.ReadLine();
